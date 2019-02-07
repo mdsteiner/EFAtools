adacc82588c08cba6421bbd1eca4960e8e1e9fa0
mdsteiner <markus.steiner@unibas.ch>
2019-02-07 13:19:23 +0100

    updated spss output lists and compare function

--- R/compare.R
+++ R/compare.R
@@ -1,16 +1,38 @@
-#' Compare loadings of different implementations
+#' Compare loadings or communalities of different implementations
 #'
-#' @param x matrix or dataframe. Loadings of one implementation.
-#' @param y matrix or dataframe. Loadings of another implementation to compare to
-#' x.
+#' The functions takes two objects of the same dimensions containing numeric
+#' information (loadings or communalities) and prints the following summary
+#' information of the absolute difference of the objects: mean, median, min, max,
+#' number above which the max decimals to round to where
+#' all corresponding elements of x and y are still equal are displayed, and the
+#' difference object itself.
+#'
+#' @param x matrix, dataframe, or vector. Loadings or communalities of one
+#'  implementation.
+#' @param y matrix, dataframe, or vector. Loadings or communalities of another
+#'  implementation to compare to x.
 #' @param reorder logical. Whether factors should be reordered according to their
 #'   colnames.
 #' @param print_digits numeric. Number of decimals to print in the output
 #'  (default is 4).
+#' @param m_red numeric. Number above which the mean and median should be printed
+#'  in red (i.e., if .001 is used, the mean will be in red if it is larger than
+#'  .001, otherwise it will be displayed in green. Default is .001).
+#' @param range_red numeric. Number above which the min and max should be printed
+#'  in red (i.e., if .001 is used, min and max will be in red if the max is larger
+#'   than .001, otherwise it will be displayed in green. Default is .001). Note that
+#'   the color of min also depends on max.
+#' @param round_red  numeric. Number above which the max decimals to round to where
+#' all corresponding elements of x and y are still equal are displayed in red
+#' (i.e., if 3 is used, the number will be in red if it is smaller than
+#'  3, otherwise it will be displayed in green. Default is 3).
+#' @param print_diff logical. Whether the difference vector or matrix should be
+#'  printed or not (default is TRUE).
 #'
 #' @return Print out a comparison summary.
 #' @export
-compare <- function(x, y, reorder = TRUE, print_digits = 4) {
+compare <- function(x, y, reorder = TRUE, print_digits = 4, m_red = .001,
+                    range_red = .001, round_red = 3, print_diff = TRUE) {
 
   if (isTRUE(reorder) && !(class(x) %in% c("numeric", "COMMUNALITIES")) &&
       !(class(y) %in% c("numeric", "COMMUNALITIES"))) {
@@ -47,14 +69,14 @@ compare <- function(x, y, reorder = TRUE, print_digits = 4) {
 
   # prepare to print statistics
 
-  if (mean_abs_diff <= .001) {
+  if (mean_abs_diff <= m_red) {
     mean_out <- crayon::green$bold(.numformat(mean_abs_diff, print_digits,
                                               TRUE))
   } else {
     mean_out <- crayon::red$bold(.numformat(mean_abs_diff, print_digits, TRUE))
   }
 
-  if (median_abs_diff <= .001) {
+  if (median_abs_diff <= m_red) {
     median_out <- crayon::green$bold(.numformat(median_abs_diff, print_digits,
                                                 TRUE))
   } else {
@@ -62,25 +84,23 @@ compare <- function(x, y, reorder = TRUE, print_digits = 4) {
                                               TRUE))
   }
 
-  if (min_abs_diff <= .00001) {
+  if (max_abs_diff <= range_red) {
+    max_out <- crayon::green$bold(.numformat(max_abs_diff, print_digits, TRUE))
     min_out <- crayon::green$bold(.numformat(min_abs_diff, print_digits, TRUE))
   } else {
-    min_out <- crayon::red$bold(.numformat(min_abs_diff, print_digits, TRUE))
-  }
-
-  if (max_abs_diff <= .001) {
-    max_out <- crayon::green$bold(.numformat(max_abs_diff, print_digits, TRUE))
-  } else {
     max_out <- crayon::red$bold(.numformat(max_abs_diff, print_digits, TRUE))
+    min_out <- crayon::red$bold(.numformat(min_abs_diff, print_digits, TRUE))
   }
 
   if (length(are_equal) == 0) {
     equal_out <- crayon::red$bold("none")
-  } else if (are_equal < 3) {
+  } else if (are_equal < round_red) {
     equal_out <- crayon::red$bold(are_equal)
   } else {
     equal_out <- crayon::green$bold(are_equal)
   }
+
+
 
   cat("Mean [min, max] absolute difference: ")
   cat(paste0(mean_out, " [", min_out, ", ", max_out, "]"))
@@ -89,5 +109,24 @@ compare <- function(x, y, reorder = TRUE, print_digits = 4) {
   cat("\n")
   cat(paste0("Max decimals to round to where loadings are equal: ",
              equal_out))
+
+  # create the difference object
+  if (isTRUE(print_diff)) {
+    cat("\n")
+    cat("\n")
+    if (any(class(x) == "LOADINGS") || any(class(y) == "LOADINGS")) {
+      class(diff) <- "LOADINGS"
+      print(diff, digits = print_digits)
+    } else if (any(class(x) == "COMMUNALITIES") ||
+               any(class(y) == "COMMUNALITIES")) {
+
+      class(diff) <- "COMMUNALITIES"
+      print(diff, digits = print_digits)
+
+    } else {
+      print(round(diff, digits = print_digits))
+    }
+
+  }
 
 }
