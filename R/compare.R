ad9bcbd9c4d7d18168f5d0149added739210fb9a
mdsteiner <markus.steiner@unibas.ch>
2020-02-02 18:25:03 +0100

    updated documentation compare function

--- R/compare.R
+++ R/compare.R
@@ -12,6 +12,8 @@
 #' @param reorder logical. Whether elements / columns should be reordered.
 #'  (loading) matrices are reordered according to Tuckers correspondence coefficient,
 #'  other objects according to the names.
+#' @param corres logical. Whether factor correspondences should be compared if a matrix
+#'  is entered.
 #' @param thresh numeric. The threshold to classify a pattern coefficient as substantial. Default is .3.
 #' @param digits numeric. Number of decimals to print in the output (default is 4).
 #' @param m_red numeric. Number above which the mean and median should be printed
