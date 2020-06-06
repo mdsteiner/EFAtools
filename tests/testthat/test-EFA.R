ea5e73c88fbe04258ec2d90d4abee59fb4d834db
Silvia Grieder <silvia.grieder@unibas.ch>
2020-06-06 18:32:15 +0200

    Create test-EFA.R

---
+++ tests/testthat/test-EFA.R
@@ -0,0 +1,6 @@
+efa_cor <- EFA(test_models$baseline$cormat, n_factors = 3, N = 500)
+efa_raw <- EFA(GRiPS_raw, n_factors = 1)
+
+set.seed(500)
+# HIER STEHENGEBLIEBEN --> evtl. rumspielen mit 4, 3, 2 indicators
+efa_rand <- SMT(matrix(rnorm(100), ncol = 4))
