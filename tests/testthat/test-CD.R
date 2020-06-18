34bc760ab80d4a4563a4825db3a74a9fe38e7ad7
mdsteiner <markus.steiner@unibas.ch>
2020-06-18 07:35:42 +0200

    Update test-CD.R

--- tests/testthat/test-CD.R
+++ tests/testthat/test-CD.R
@@ -4,10 +4,12 @@ test_that("output class and dimensions are correct", {
   expect_is(cd_grips, "CD")
   expect_named(cd_grips, c("n_factors", "eigenvalues", "RMSE_eigenvalues",
                            "settings"))
+  expect_is(cd_grips$RMSE_eigenvalues, "matrix")
 })
 
 test_that("CD returns the correct values", {
   expect_equal(cd_grips$n_factors, 1)
+  expect_equal(sum(cd_grips$eigenvalues), 8)
 })
 
 test_that("errors etc. are thrown correctly", {
@@ -15,8 +17,6 @@ test_that("errors etc. are thrown correctly", {
   expect_error(CD(test_models$baseline$cormat), " 'x' is a correlation matrix, but CD only works with raw data.")
 
   expect_warning(CD(GRiPS_raw, n_factors_max = 5), " n_factors_max was set to 5 but maximum possible factors to extract is 4 . Setting n_factors_max to 4 .")
-
-
-
 })
 
+rm(cd_grips)
