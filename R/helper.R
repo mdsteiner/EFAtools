697176b15682e218a09b3f1aac5c94d0a81afc73
andreassoteriadesmoj <91131120+andreassoteriadesmoj@users.noreply.github.com>
2026-03-20 09:25:55 +0000

    closes #75

--- R/helper.R
+++ R/helper.R
@@ -97,10 +97,45 @@
 
 #' Confidence intervals around mean
 #'
-#' This function is used internally by \code{\link{EFA_POOLED}} to calculates
+#' This function is used internally by \code{\link{EFA_POOLED}} to calculate
 #' confidence intervals (CIs) around the pooled loadings and pooled interfactor
 #' correlations.
 #'
+#' @details
+#' The standard error (SE) to use in the creation of the CIs is calculated
+#' according to Rubin's (1987) formula (eq. 11 in Hayes & Enders, 2023):
+#'
+#' \eqn{\sqrt{mean(SE^2) + var(\hat{\theta_{m}}) + var(\hat{\theta_{m}}) / M}}
+#'
+#' According to Hayes & Enders (2023) p. 42:
+#'
+#' \emph{[T]he first term under the radical represents the average squared
+#' standard error (the within imputation sampling variance [...]), the second
+#' term depends on the variance of the \emph{M} parameter estimates around their
+#' average (the between imputation variance [...]), and the final term
+#' represents the squared standard error of the pooled estimate [...].
+#' Conceptually, the first term estimates the sampling error of a complete-data
+#' analysis, and the next two terms are essentially correction factors that
+#' inflate the standard error to compensate for uncertainty due to the
+#' imputations–that is, additional uncertainty (sampling variability) in the
+#' parameter estimates caused by missing data.}
+#'
+#' Currently, it is not possible to calculate the first term, because
+#' \code{\link{EFA}} does not calculate SEs for the loadings. Only the second
+#' and third terms are used in the calculation of SE for the CIs.
+#'
+#' The CI is generally calculated as:
+#'
+#' CI = Point estimate ± Margin of error,
+#'
+#' where
+#'
+#' Margin of error = Critical value × SE of point estimate.
+#'
+#' To account for situations where the sample size is small, instead of using
+#' z-values, the critical value is derived from the \emph{t} distribution with
+#' \code{n - 1} degrees of freedom (Hazra, 2017).
+#
 #' @param means A matrix with the pooled loadings or pooled interfactor
 #' correlations
 #' @param sds A matrix with the standard deviations for the loadings or
@@ -108,11 +143,30 @@
 #' @param p Numeric. One minus the confidence level of the CIs. Defaults to 0.05
 #' for 95\% CIs.
 #' @param n Numeric. Typically, the number of permutations \eqn{m}. See
-#' \code{\link{EFA_POOLED}}
+#' \code{\link{EFA_POOLED}}.
 #'
 #' @return A list with the lower and upper CIs.
+#'
+#' @references
+#' Hayes, T. & Enders, C. K. (2023). Maximum likelihood and multiple imputation
+#' missing data handling: how they work, and how to make them work in practice.
+#' In \emph{APA Handbook of Research Methods in Psychology, Second Edition} Vol.
+#' 3. Data Analysis and Research Publication, H. Cooper (Editor-in-Chief).
+#'
+#' Hazra, A. (2017). Using the confidence interval confidently. \emph{Journal of
+#' Thoracic Disease} 9(10), 4125--4130.
+#'
+#' Rubin, D. B. (1987). Multiple imputation for nonresponse in surveys. Wiley.
+#' \url{https://doi.org/10.1002/9780470316696}
 .calc_cis <- function(means, sds, p = 0.05, n) {
-  error <- qnorm(1 - p / 2) * sds / sqrt(n)
+
+  # Calculate standard error
+  rubin_term_1 <- 0 # Currently not calculated- see Details
+  rubin_term_2 <- sds ^ 2 # The variances
+  rubin_term_3 <- rubin_term_2 / n
+  se <- sqrt(rubin_term_1 + rubin_term_2 + rubin_term_3)
+
+  error <- qt(1 - p / 2, n - 1) * se
 
   lower_ci <- means - error
   upper_ci <-  means + error
